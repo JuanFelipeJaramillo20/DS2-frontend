a18a5a598ac149003820e760878bf0a9b09ebd5c
Juan David Moreno Alfonso <juandavid@cressco.dev>
2023-12-10 11:28:54 -0500

    nav

--- src/components/Nav/Index.jsx
+++ src/components/Nav/Index.jsx
@@ -1,8 +1,37 @@
+import { useEffect } from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
 
 import "./style.css";
 
 const Nav = () => {
+  localStorage.setItem("userId", "1");
+  localStorage.setItem(
+    "token",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MDIyMzEyNjEsInVzZXJfaWQiOjF9.AjKmKEWal7kghbtQzKnQds8BRNyxzNy9AibEQYvPqJw"
+  );
+  const userId = localStorage.getItem("userId");
+  const token = localStorage.getItem("token");
+  useEffect(() => {
+    const BASE_URL = import.meta.env.VITE_BASE_URL;
+    const url = `${BASE_URL}/user/${userId}`;
+    const headers = {
+      Authorization: `${token}`,
+      "Content-Type": "application/json",
+    };
+    const fetchUserInformation = async () => {
+      try {
+        const response = await axios.get(url, { headers: headers });
+        if (response.data) {
+          sessionStorage.setItem("userName", response.data.Name);
+          sessionStorage.setItem("userPhone", response.data.Phone);
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    fetchUserInformation();
+  }, [token, userId]);
   return (
     <nav className="nav-bar__container">
       <section>
